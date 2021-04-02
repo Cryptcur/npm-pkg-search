a182c135cd2a5b7e9aba0fac67bfe11058cb5fcc
Jonathan Cordero <cryptcur00101@protonmail.com>
2021-04-02 17:58:42 -0400

    action calls successful

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+
 const RepositoriesList: React.FC = () => {
+    const [term, setTerm] = useState('');
+    const { searchRepositories } = useActions();
+    const onSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
+        ev.preventDefault()
+        searchRepositories(term)
+    }
     return <div>
-        <form>
-            <input />
+        <form onSubmit={onSubmit}>
+            <input value={term} onChange={(ev) => setTerm(ev.target.value)} />
             <button>Button</button>
         </form>
     </div>
